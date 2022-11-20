0265cd976f5107e7a4f4c096442a877a11ca533d
mawa <mawahama05@gmail.com>
2022-11-19 20:54:53 -0800

    completed

--- src/index.js
+++ src/index.js
@@ -4,20 +4,3 @@ import "./index.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-function Article() {
-    return (
-      <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-    );
-  }
-  function Comment() {
-    return <div>Naturally, I agree with this article.</div>;
-  }
-  function App() {
-    return (
-      <div>
-        <Article />
-        <Comment />
-      </div>
-    );
-  }
